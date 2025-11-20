ca429798a51fba43bf0922bebe8b10fcfa67ccf8
Bertram Jensen <bertram.pinstrup.jensen@outlook.com>
2025-11-20 13:52:44 +0100

    TestScript

---
+++ Card game/Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Keycode drawCard;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
